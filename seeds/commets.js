f517ced4d513a2f37139bd76e52ade107e045383
Aaron Sando <Aaron@Charactercabinets.com.au>
2021-11-29 10:34:16 +0800

    adding some basic seed data

---
+++ seeds/commets.js
@@ -0,0 +1,38 @@
+const { Comment } = require('../models');
+
+const commentData = [
+  {
+    comment_text: "Look what I added",
+    post_id: 3,
+    user_id: 1
+  },
+  {
+    comment_text: "TEXT this is just TEXT",
+    post_id: 1,
+    user_id: 4
+  },
+  {
+    comment_text: "cookies what are they good for (besides eating I mean)!",
+    post_id: 4,
+    user_id: 2
+  },
+  {
+    comment_text: "Great. Now I'm hungry for cookies.",
+    post_id: 4,
+    user_id: 3
+  },
+  {
+    comment_text: "what should I do about coding with JSON",
+    post_id: 5,
+    user_id: 5
+  },
+  {
+    comment_text: "Yeah! Adding random data is a great way to force a unique hashed output",
+    post_id: 5,
+    user_id: 4
+  },
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
