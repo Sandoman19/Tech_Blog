44e5c2a1613517e0870dd2fd376b9b53239652a0
Aaron Sando <Aaron@Charactercabinets.com.au>
2021-12-20 07:48:12 +0800

    signup.js added

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+async function signupFromHandler(event) {
+
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').ariaValueMax.trim();
+  const password = document.querySelector('#password-signup').ariaValueMax.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    if (response.ok) {
+
+      console.log('success');
+
+      document.location.replace('/dashboard');
+
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
